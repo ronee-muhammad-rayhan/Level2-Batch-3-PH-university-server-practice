5ebb708a6d0ca4bd4077fd62392c0c90b53db4a9
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-06-07 20:56:25 +0600

    HigherOrderFunction

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,13 +3,22 @@ import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
-const shenaBahini = (req: Request, res: Response, next: NextFunction) => {
-//   console.log('i am a shenabahini');
-  console.log(req.body);
-  next();
-  //   next();
+const shenaBahini = (name) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    //   console.log('i am a shenabahini');
+    //   console.log(req.body);
+    console.log(`i am a shenabahini and my name is ${name}`);
+
+    //   validation
+    // next();
+    //   next();
+  };
 };
 
-router.post('/create-student', shenaBahini, UserControllers.createStudent);
+router.post(
+  '/create-student',
+  shenaBahini('validateRequest'),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
