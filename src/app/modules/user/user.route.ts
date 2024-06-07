169f6a6a4e55725b2e3890390b5c11a8bee9b96b
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-06-07 22:14:17 +0600

    12-3 Implement ValidateRequest Middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,25 +1,9 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import { UserControllers } from './user.controller';
-import { AnyZodObject } from 'zod';
 import { createStudentValidationSchema } from '../student/student.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
-
-const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      //   validation check
-      // if everything allright next() ->
-      await schema.parseAsync({
-        body: req.body,
-      });
-
-      return next();
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 router.post(
   '/create-student',
