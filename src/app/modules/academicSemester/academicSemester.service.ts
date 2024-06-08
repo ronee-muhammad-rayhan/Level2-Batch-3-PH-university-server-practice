677b5207dcfe9d76715ed80fb142259d5e903d01
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-06-08 17:59:30 +0600

    12-8 Handle Logical Validation Of Academic Semester

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,18 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constants';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  //   business logic
+  // mapping
+  // mapper object
+  // semester name --> semester code
+
+  // academicSemesterNameCodeMapper['Fall]
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid semester code');
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
