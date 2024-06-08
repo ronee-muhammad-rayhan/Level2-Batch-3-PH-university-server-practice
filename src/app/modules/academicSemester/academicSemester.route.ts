04cf7576d5806a2064894c13e864d66c39ba07fd
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-06-08 16:44:16 +0600

    12-7 Create Academic Semester Service

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidations } from './academicSemester.validation';
 
 const router = express.Router();
 router.post(
   '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidations.createAcademicSemesterValidationSchema,
+  ),
   AcademicSemesterController.createAcademicSemester,
 );
 export const AcademicSemesterRoutes = router;
